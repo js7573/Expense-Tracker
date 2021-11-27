47c2117a95c577e31d67949463d2205fc5440fe4
js7573 <js7573@student@uni-lj.si>
2021-11-27 11:11:48 +0100

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const loadEl = document.querySelector('#load');
+
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    //
+    // firebase.auth().onAuthStateChanged(user => { });
+    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+    // firebase.firestore().doc('/foo/bar').get().then(() => { });
+    // firebase.functions().httpsCallable('yourFunction')().then(() => { });
+    // firebase.messaging().requestPermission().then(() => { });
+    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+    // firebase.analytics(); // call to activate
+    // firebase.analytics().logEvent('tutorial_completed');
+    // firebase.performance(); // call to activate
+    //
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    try {
+      const app = firebase.app();
+      const db = firebase.firestore();
+      let myUser = db.collection('Users').doc('User1');
+
+      myUser.get().then(doc => {
+        const data = doc.data();
+        console.log(data.Username);
+        console.log(data.Password);
+      });
+    } 
+    
+    catch (e) {
+      console.error(e);
+      loadEl.textContent = 'Error loading the Firebase SDK, check the console.';
+    }
+
+});
+
+function addUser(){
+    let a = 5;
+}
