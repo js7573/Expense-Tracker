a2fbe0bdead3d2f3c31cc1bc128f9b972072d0ad
js7573 <js7573@student@uni-lj.si>
2021-12-01 09:24:29 +0100

    basic login, registration, dashboard page

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,7 @@
+let app;
+let db;
+let auth;
+
 document.addEventListener('DOMContentLoaded', function() {
 
     const loadEl = document.querySelector('#load');
@@ -18,15 +22,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
     try {
-      const app = firebase.app();
-      const db = firebase.firestore();
-      let myUser = db.collection('Users').doc('User1');
+      app = firebase.app();
+      db = firebase.firestore();
+      auth = firebase.auth();
 
-      myUser.get().then(doc => {
+      /*
+        myUser.get().then(doc => {
         const data = doc.data();
         console.log(data.Username);
         console.log(data.Password);
-      });
+        });
+      */
+      let myUser = db.collection('Users').doc('User1');
     } 
     
     catch (e) {
@@ -36,6 +43,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
-function addUser(){
-    let a = 5;
+async function addUser(){
+  let firstName = document.getElementById("exampleFirstName").value;
+  let lastName = document.getElementById("exampleLastName").value;
+  let email = document.getElementById("exampleInputEmail").value;
+  let password = document.getElementById("exampleInputPassword").value;
+  let repeatPassword = document.getElementById("exampleRepeatPassword").value
+
+  if(password === repeatPassword){
+    const userData = {
+      FirstName: firstName,
+      LastName: lastName,
+      Email: email,
+      Password: password
+    };
+
+    // Add user to firebase authentication to allow login
+    auth.createUserWithEmailAndPassword(email, password).then(credentials => {
+      alert("User created:\n" + credentials);
+    });
+    
+    // Add a new document in collection "users" with email as ID
+    const res = await db.collection('Users').doc(email).set(userData);
+  }
+}
+
+async function signIn(){
+  let email = document.getElementById("exampleInputEmail").value;
+  let password = document.getElementById("exampleInputPassword").value;
+  auth.signInWithEmailAndPassword(email, password).then(credentials => {
+  // Signed in 
+  const user = credentials.user;
+  window.location.replace("/dashboard.html");
+  });
 }
