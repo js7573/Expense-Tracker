f127990ba23f5fad47276d061218490d31ce87a9
js7573 <js7573@student@uni-lj.si>
2021-12-16 11:40:38 +0100

    add expenses insert, show correct user after login

--- public/js/app.js
+++ public/js/app.js
@@ -12,10 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
     //
     firebase.auth().onAuthStateChanged(user => { 
       if (user) {
-        console.log("Signed in");
         signedInUser = user;
       } else {
-        console.log("Not signed in");
         signedInUser = null;
       }
       checkIfSignedIn();
@@ -42,8 +40,9 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(data.Username);
         console.log(data.Password);
         });
+
+        let myUser = db.collection('Users').doc('User1');
       */
-      let myUser = db.collection('Users').doc('User1');
     } 
     
     catch (e) {
@@ -94,11 +93,61 @@ async function signOut(){
 }
 
 async function checkIfSignedIn(){
-  if(signedInUser)
-    console.log("USER FOUND");
+  if(signedInUser){
+    let email = signedInUser.email;
+    let user = db.collection('Users').doc(email);
+
+    user.get().then(doc => {
+      const data = doc.data();
+      let firstName = data.FirstName;
+      let lastName = data.LastName;
+      let email = data.Email;
+      let balance = data.Balance;
+      document.getElementsByClassName("mr-2 d-none d-lg-inline text-gray-600 small")[0].textContent = firstName + " " + lastName;
+
+      let currentSite = window.location.href.split("/");
+      currentSite = currentSite[currentSite.length - 1];
+
+      switch(currentSite){
+        case "dashboard-new.html":
+          dashboardCode(balance);
+          break;
+        case "addExpense.html":
+          addExpenseCode();
+      }
+    }); 
+  }
   else{
     let currentFile = window.location.href.split("/").slice(-1)[0];
     if(currentFile !== "index.html" && currentFile !== "register.html" && currentFile !== "404.html")
       window.location.replace("/index.html");
   }
+}
+
+async function dashboardCode(balance){
+  document.getElementsByClassName("total-amount-spent")[0].innerHTML = "<h1>" + balance + " €</h1>";
+}
+
+async function addExpenseCode(){
+
+}
+
+async function addExpense(){
+  let expenseName = document.getElementById("expenseName").value;
+  let expenseAmount = document.getElementById("expenseAmount").value;
+  let expenseDate = document.getElementById("expenseDate").value;
+  let expenseCategory = document.getElementById("expenseCategories").value;
+  let expenseDescription = document.getElementById("expenseDescription").value;
+  let expenseID = 2;
+  const expenseData = {
+    Name: expenseName,
+    Amount: expenseAmount,
+    Date: expenseDate,
+    CategoryID: expenseCategory,
+    Description: expenseDescription,
+    UserID: signedInUser.email,
+    ExpenseID: 2,
+  };
+
+  const res = await db.collection('Expenses').doc(expenseName).set(expenseData);
 }
